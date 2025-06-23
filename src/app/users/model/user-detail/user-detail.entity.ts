667e376008b08377883eb2dc0218653252039d6b
marckszz <mateocab100@gmail.com>
2025-06-22 20:35:38 -0500

    Update user-detail.entity.ts

--- src/app/users/model/user-detail/user-detail.entity.ts
+++ src/app/users/model/user-detail/user-detail.entity.ts
@@ -1,1 +1,19 @@
 
+export class UserDetail {
+  id: string;
+  userId: string;
+  favorites: string[];
+  viewed: string[];
+
+  constructor(detail: {
+    id?: string;
+    userId?: string;
+    favorites?: string[];
+    viewed?: string[];
+  }) {
+    this.id = detail.id || '';
+    this.userId = detail.userId || '';
+    this.favorites = detail.favorites || [];
+    this.viewed = detail.viewed || [];
+  }
+}
