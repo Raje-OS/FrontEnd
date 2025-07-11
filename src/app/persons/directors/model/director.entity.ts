81556d47f2cd50cee066ff6e70b24d48cf46ae11
RIZE <mateocab100@gmail.com>
2025-07-11 02:46:35 -0500

    esteticos

--- src/app/persons/directors/model/director.entity.ts
+++ src/app/persons/directors/model/director.entity.ts
@@ -2,7 +2,7 @@ export class Director {
   id: string;
   nombre: string;
   descripcion: string;
-  fehcaNacimiento: string;
+  fechaNacimiento: string;
   ciudad_origen: string;
   imagen: string;
 
@@ -17,7 +17,7 @@ export class Director {
     this.id = director.id || '';
     this.nombre = director.nombre || '';
     this.descripcion = director.descripcion || '';
-    this.fehcaNacimiento = director.fechaNacimiento || '';
+    this.fechaNacimiento = director.fechaNacimiento || '';
     this.ciudad_origen = director.ciudad_origen || '';
     this.imagen = director.imagen || '';
   }
